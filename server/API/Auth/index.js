bb1d13412177e9658a739388f081c0bbc52de3c2
akanksha3012 <burnwal.akanksha30@gmail.com>
2021-09-21 21:55:45 +0530

    code modified using statics and methods in mongoose

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -1,6 +1,6 @@
 // Library
 import express from 'express';
-import bcryptjs from 'bcryptjs';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 //Model
@@ -17,26 +17,11 @@ Method          post
 */
 Router.post("/signup", async (req, res) => {
     try{
-        const {email, password, fullName, phoneNumber} = req.body.credentials;
-        const checkUserByEmail = await UserModel.findOne({email});
-        const checkUserByPhone = await UserModel.findOne({phoneNumber});
+        await UserModel.findByEmailAndPhone(req.body.credentials);
 
-        if(checkUserByEmail || checkUserByPhone){
-            return res.json({email: "user already exists!"});
-        }
-        //hash password
-        const bcryptSalt = await bcryptjs.genSalt(8);
-        // await is used as salting takes time
-        const hashedPassword = await bcryptjs.hash(password, bcryptSalt);
+        const newUser = await UserModel.create(req.body.credentials);
 
-        // save to database
-        await UserModel.create({
-            ...req.body.credentials, 
-            password: hashedPassword,
-        });
-
-        // generate JWT Auth tokens
-        const token = jwt.sign({ user: {fullName, email}}, "ZomatoApp");
+        const token = newUser.generateJwtToken();
 
         return res.status(200).json({token, status:"success"});
 
@@ -44,5 +29,25 @@ Router.post("/signup", async (req, res) => {
         return res.status(500).json({error:error.message });
     }
 });
+
+/*
+Route           /auth/signin
+Desc            Signin with email and password
+Params          none
+Access          public
+Method          post
+*/
+Router.post("/signin", async (req, res) => {
+    try{
+        const user = await UserModel.findByEmailAndPassword(req.body.credentials);
+        const token = user.generateJwtToken();
+
+        return res.status(200).json({token, status:"success"});
+    }
+    catch(error){
+        return res.status(500).json({error:error.message });
+    }
+});
+
 
 export default Router;
