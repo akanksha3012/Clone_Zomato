abca25ee9e9238df4b9ba2a77954e343de59ce1a
akanksha3012 <burnwal.akanksha30@gmail.com>
2021-11-07 23:36:23 +0530

    overview page

--- client/src/Layout/Restaurant.layout.jsx
+++ client/src/Layout/Restaurant.layout.jsx
@@ -1,8 +1,15 @@
+//libraries
 import React,{useState} from 'react'
-
+//components
 import RestaurantNavbar from '../Components/Navbar/RestaurantNavbar';
 import ImageGrid from '../Components/Restaurant/ImageGrid';
+import InfoButtons from '../Components/Restaurant/InfoButtons';
 import RestaurantInfo from '../Components/Restaurant/RestaurantInfo';
+
+import { TiStarOutline } from 'react-icons/ti';
+import { RiDirectionLine, RiShareForwardLine } from 'react-icons/ri';
+import { BiBookmarkPlus } from 'react-icons/bi';
+import Tabs from '../Components/Restaurant/Tabs';
 
 function RestaurantLayout({children}) {
     const [restaurant, setRestaurant] = useState({
@@ -20,9 +27,20 @@ function RestaurantLayout({children}) {
             <div className="container mx-auto px-4 lg:px-20 pb-10">
                 <ImageGrid images={restaurant.images} />
                 <RestaurantInfo name={restaurant?.name} restaurantRating={restaurant?.restaurantRating || 0} deliveryRating={restaurant?.deliveryRating || 0} cuisine={restaurant?.cuisine} address={restaurant?.address}/>
+                
+            <div className="my-4 flex flex-wrap gap-3">
+                <InfoButtons isActive>
+                    <TiStarOutline/> Add Review
+                </InfoButtons>
+                <InfoButtons><RiDirectionLine/>Direction</InfoButtons>
+                <InfoButtons><BiBookmarkPlus/>Bookmark</InfoButtons>
+                <InfoButtons><RiShareForwardLine/>Share</InfoButtons>
             </div>
-           
+            <div className="my-10">
+                <Tabs/>
+            </div>
             {children}
+            </div>
         </>
     )
 }
---
+++ client/src/pages/Overview.jsx
@@ -0,0 +1,175 @@
+import React,{useState} from 'react';
+import {Link, useParams} from 'react-router-dom';
+import {IoMdArrowDropright} from 'react-icons/io';
+import Slider from 'react-slick';
+import ReactStars from 'react-rating-stars-component';
+//components
+import {NextArrow, PrevArrow} from '../Components/CarouselArrows';
+import MenuCollection from '../Components/Restaurant/MenuCollection';
+import MenuSimilarRestaurantCard from '../Components/Restaurant/MenuSimilarRestaurantCard';
+import ReviewCard from '../Components/Restaurant/ReviewCard';
+
+function Overview() {
+    const {id} = useParams();
+
+    const [menuImage, setMenuImage] = useState([
+       "https://b.zmtcdn.com/data/pictures/0/18930190/8b41602a27ac441ba909237947be73ec.jpg",
+       "https://b.zmtcdn.com/data/reviews_photos/faf/e76d6bc3df21781ca49c93dd4d4edfaf_1624699914.jpg",
+       "https://b.zmtcdn.com/data/dish_photos/e56/c6942fc1a954329fe4ed8bdf9a481e56.jpg"
+
+    ]);
+    const [Reviews, setReviews] = useState([
+        {
+            isRestaurantReview: true,
+            createdAt:"2020-06-01T12:00:00.0002",
+            reviewText: "This is a must visit.",
+        },
+        {
+            isRestaurantReview: false,
+            createdAt:"2020-06-01T12:00:00.0002",
+            reviewText: "This is a must visit.",
+        },
+        {
+            isRestaurantReview: true,
+            createdAt:"2020-06-01T12:00:00.0002",
+            reviewText: "This is a must visit.",
+        },
+        {
+            isRestaurantReview: false,
+            createdAt:"2020-06-01T12:00:00.0002",
+            reviewText: "This is a must visit.",
+        },
+    ]);
+
+    const cuisines = ["North Indian", "chinese", "Italian"];
+    const averageCost = 100;
+
+    const ratingChanged = (newRating) => {
+        console.log(newRating);
+    }
+
+    const settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        initialSlide: 0,
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToScroll: 3,
+              slidesToShow: 3,
+              infinite: false,
+              dots: true,
+            },
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToScroll: 2,
+              slidesToShow: 2,
+            },
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToScroll: 1,
+              slidesToShow: 1,
+              dots: false,
+            },
+          },
+        ],
+      };
+    return (
+        <>
+        <div className="flex flex-col md:flex-row relative gap-6">
+        <div className="w-full md:w-8/12">
+          <h2 className="font-semibold text-lg md:text-xl my-4">
+            About this place
+          </h2>
+          <div className="flex justify-between items-center">
+            <h4 className="text-lg font-medium">Menu</h4>
+            <Link to={`/restaurant/${id}/menu`}>
+              <span className="flex items-center gap-1 text-zomato-400">
+                See all menu <IoMdArrowDropright />
+              </span>
+            </Link>
+          </div>
+          <div className="flex flex-wrap gap-3 my-4">
+           <MenuCollection menuTitle='Menu' pages='3' image={menuImage}/>
+          </div>
+          <h4 className="text-lg font-medium my-4">Cuisines</h4>
+          <div className="flex flex-wrap gap-2">
+           {cuisines.map(data => (
+               <span className="border border-gray-600 text-gray px-2 py-1 rounded-full">{data}</span>
+           ))}
+          </div>
+          <div className="my-4">
+            <h4 className="text-lg font-medium">Average Cost</h4>
+            <h6>${averageCost} for one order (approx.)</h6>
+            <small className="text-gray-500">
+              Exclusive of applicable taxes and service charges, if any
+            </small>
+          </div>
+          <div className="my-4">
+            <h4 className="text-lg font-medium">Similar Restaurants</h4>
+            <div>
+              <Slider {...settings}>
+              <MenuSimilarRestaurantCard
+                  image="https://b.zmtcdn.com/data/pictures/chains/1/18216901/c9028cccf313b8a09996ea30e3fe8526_featured_v2.jpg"
+                  title="Unplugged Courtyard"
+                />
+                <MenuSimilarRestaurantCard
+                  image="https://b.zmtcdn.com/data/pictures/chains/1/18216901/c9028cccf313b8a09996ea30e3fe8526_featured_v2.jpg"
+                  title="Unplugged Courtyard"
+                />
+                <MenuSimilarRestaurantCard
+                  image="https://b.zmtcdn.com/data/pictures/chains/1/18216901/c9028cccf313b8a09996ea30e3fe8526_featured_v2.jpg"
+                  title="Unplugged Courtyard"
+                />
+                <MenuSimilarRestaurantCard
+                  image="https://b.zmtcdn.com/data/pictures/chains/1/18216901/c9028cccf313b8a09996ea30e3fe8526_featured_v2.jpg"
+                  title="Unplugged Courtyard"
+                />
+                <MenuSimilarRestaurantCard
+                  image="https://b.zmtcdn.com/data/pictures/chains/1/18216901/c9028cccf313b8a09996ea30e3fe8526_featured_v2.jpg"
+                  title="Unplugged Courtyard"
+                />
+                <MenuSimilarRestaurantCard
+                  image="https://b.zmtcdn.com/data/pictures/chains/1/18216901/c9028cccf313b8a09996ea30e3fe8526_featured_v2.jpg"
+                  title="Unplugged Courtyard"
+                />
+              </Slider>
+            </div>
+          </div>
+          <div className="mb-4 mt-8">
+              <h4 className="text-lg font-medium">Rate your delivery experience</h4>
+              <ReactStars
+              count={5}
+              onChange={ratingChanged}
+              size={24}
+              activeColor="#ffd700"
+            />
+               {Reviews.map((reviewData) => (
+              <ReviewCard {...reviewData} />
+            ))}
+          </div>
+          <div className="my-4 w-full md:hidden flex flex-col gap-4">
+              ...Map stuff
+          </div>
+          </div> 
+          <aside style={{height: 'fit-content'}} className="hidden md:flex md:w-4/12 sticky rounded-xl top-2 bg-white p-3 shadow-md flex-col gap-4">
+              ...Map Stuff
+          </aside>
+
+        </div>  
+        
+        </>
+    );
+}
+
+export default Overview;
